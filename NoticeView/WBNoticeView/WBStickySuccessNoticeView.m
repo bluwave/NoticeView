f3a88f181de31bff7a762c37aab252bc9b574a61
Garrett Richards <grichards@seamlessweb.com>
2012-06-12 11:46:27 -0600

    added WBStickySuccessView

---
+++ NoticeView/WBNoticeView/WBStickySuccessNoticeView.m
@@ -0,0 +1,16 @@
+//
+// Created by slim on 6/12/12.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "WBStickySuccessNoticeView.h"
+
+
+@implementation WBStickySuccessNoticeView
+{
+
+}
+
+@end
